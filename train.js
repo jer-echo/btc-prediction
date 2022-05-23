5df7b09d597c87040b3438a0e49bf251e94b08e2
jer-echo <aquino.jericho20@gmail.com>
2022-05-23 09:10:57 +0000

    up

--- train.js
+++ train.js
@@ -2,18 +2,47 @@ const brain = require('brain.js');
 const data = require('./close.json');    
 const fs = require("fs");
 
-const net = new brain.recurrent.LSTMTimeStep();
+const NET_FILE = 'net.json'
+const PAGE_FILE = 'page.json'
 
-net.train([data],{
-    "iterations": 1,
-    "errorThresh": 0.1,
-    "log": true,
-    "logPeriod": 1,
-    "learningRate": 0.3,
-    "momentum": 0.1,
-    "callbackPeriod": 1,
-    "timeout": null
-});
+function write(f, obj) {
+    return fs.writeFileSync(f, JSON.stringify(obj), "utf-8")
+}
+    
+function exists(f) {
+    return fs.existsSync(f) 
+}
 
-const netState = net.toJSON();
-fs.writeFileSync("net.json",  JSON.stringify(netState), "utf-8");
+function read(f, d) {
+    try {
+        return exists(f) ? fs.readFileSync(f, {encoding:'utf8', flag:'r'}) || d : d
+    } catch (e ){
+        return d
+    }
+}
+
+function chucks(data, chuckSize, startPage,iterate = null) {
+    const total_index = data.length;
+    const total_pages = Math.ceil(total_index/chuckSize)
+    const res = []
+    for (let i = startPage; i <= total_pages; i++) {
+        const start_i = (i * chuckSize) - chuckSize
+        const end_i = (i * chuckSize) > total_index ? total_index : (i * chuckSize)
+        const chuck = Array.from(data).slice(start_i, end_i)
+        res.push(chuck)
+        if(typeof iterate === 'function') iterate(chuck,i,total_pages)
+    }
+    return res
+}
+const start_page = JSON.parse(read(PAGE_FILE, '{"page":1}')).page
+
+chucks(data, 5, start_page, function(chuck,page,total){
+    console.log(`status: ${page} of ${total}`)
+    const net = new brain.recurrent.LSTMTimeStep();
+    const json = JSON.parse(read(NET_FILE, null))
+    if(json) net.fromJSON(json);
+    net.train([chuck]);
+    const netState = net.toJSON();
+    write(NET_FILE, netState)
+    write(PAGE_FILE, {page})
+})
