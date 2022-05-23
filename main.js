a8289b57900dbf278d2e084abf2c7cb194d371c7
jer-echo <aquino.jericho20@gmail.com>
2022-05-23 09:22:18 +0000

    up

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', init)
 
-async function getRawData(){
-    return await (await fetch('close.json')).json()
+async function getJson(path){
+    return await (await fetch(path)).json()
 }
 
+async function getTrainedModel(){
+    return await (await fetch('close.json')).json()
+} 
+
 async function init(){
-    ;
+    const net = new brain.recurrent.LSTMTimeStep();
+    const data = await getJson('close.json')
+    const model = await getJson('net.json')
+    net.fromJSON(model)
+    const r = net.run(data.splice(0,5))
+    console.log(r)
 }
