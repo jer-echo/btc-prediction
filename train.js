c2c4b0dee4fb7d78392b4f72022db63a96aabb97
jer-echo <aquino.jericho20@gmail.com>
2022-05-23 07:18:27 +0000

    update

--- train.js
+++ train.js
@@ -3,6 +3,17 @@ const data = require('./close.json');
 const fs = require("fs");
 
 const net = new brain.recurrent.LSTMTimeStep();
-net.train([data]);
+
+net.train([data],{
+    "iterations": 1,
+    "errorThresh": 0.1,
+    "log": true,
+    "logPeriod": 1,
+    "learningRate": 0.3,
+    "momentum": 0.1,
+    "callbackPeriod": 1,
+    "timeout": null
+});
+
 const netState = net.toJSON();
 fs.writeFileSync("net.json",  JSON.stringify(netState), "utf-8");
