c2c12c23f8acb84492870194e2325364c1aa4fbb
jer-echo <aquino.jericho20@gmail.com>
2022-05-23 14:44:11 +0800

    update

--- main.js
+++ main.js
@@ -5,9 +5,5 @@ async function getRawData(){
 }
 
 async function init(){
-    const net = new brain.recurrent.LSTMTimeStep();
-    const raw = await getRawData();
-    net.train([raw])
-    console.log(raw.splice(0,5))
-    console.log(net.run(raw.splice(0,4)))
+    ;
 }
--- train.js
+++ train.js
@@ -3,10 +3,6 @@ const data = require('./close.json');
 const fs = require("fs");
 
 const net = new brain.recurrent.LSTMTimeStep();
-
-net.train(data,{
-    iterations: 1000
-});
-
+net.train([data]);
 const netState = net.toJSON();
 fs.writeFileSync("net.json",  JSON.stringify(netState), "utf-8");
